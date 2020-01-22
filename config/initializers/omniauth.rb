56168aae50b7a4a017af588c569873a0475f46ac
Frikker <stolpnikk.97@gmail.com>
2020-01-22 19:38:07 +0300

    edit authorization methods

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, ENV['VKONTAKTE_KEY'], ENV['VKONTAKTE_SECRET'],
+  provider :vkontakte, '7290933', 'zpmquqgXJJZhEQr9s9AO',
+           callback_path: '/users/omniauth_callbacks/vkontakte',
            https: 1,
            image_size: 'original'
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
