f16d4c059467207c241912f87c1165ad2051aa01
Frikker <stolpnikk.97@gmail.com>
2020-01-27 20:04:42 +0300

    added posts and comments to app(comments still doesn't work)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :photopost, dependent: :destroy
-  belongs_to :parent, class_name: Comment
+  belongs_to :parent, class_name: 'Comment'
 
   validates :user_id, presence: true
   validates :photopost_id, presence: true
