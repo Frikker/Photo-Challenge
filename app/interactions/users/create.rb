d49517baeabfe32254c81e9e8c6fa00e309369aa
Frikker <stolpnikk.97@gmail.com>
2020-02-29 00:31:03 +0300

    Add action interactions and have troubles with him

--- app/interactions/users/create.rb
+++ app/interactions/users/create.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Users
-  class CreateUser < ActiveInteraction::Base
+  class Create < ActiveInteraction::Base
     hash :auth,
          strip: false
     string :provider
