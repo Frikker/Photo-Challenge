9e03a01803bd15226085e1c9cbb3d202eb209ad9
Frikker <stolpnikk.97@gmail.com>
2020-06-03 12:32:59 +0300

    add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
+password = SecureRandom.gen_random(10)
+puts password
+AdminUser.create!(email: 'stolpnikk.97@gmail.com', password: password, password_confirmation: password)
+
 #30.times do
 # Photopost.create!(user_id: 1, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ')
 #end
