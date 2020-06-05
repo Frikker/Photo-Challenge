95a468cc9413eb06db9f8570c7f2b4bd9c11a677
Frikker <stolpnikk.97@gmail.com>
2020-06-05 16:40:03 +0300

    Finally workable likes

--- app/helpers/main_pages_helper.rb
+++ app/helpers/main_pages_helper.rb
@@ -4,10 +4,6 @@ module MainPagesHelper
     page_title.empty? ? base_title : page_title + base_title
   end
 
-  def logged_in?
-    false
-  end
-
   def new_line(photopost)
     (@photoposts.index(photopost) % 3).zero?
   end
--- app/helpers/photoposts_helper.rb
+++ app/helpers/photoposts_helper.rb
@@ -4,4 +4,18 @@ module PhotopostsHelper
   def check_likes(photopost)
     !photopost.rating.find_by(user_id: current_user).nil?
   end
+
+  def likes_icon
+    if check_likes(photopost)
+
+    end
+  end
+
+  def like_method
+
+  end
+
+  def authenticity_token
+    current_user.authenticity_token unless current_user.nil?
+  end
 end
