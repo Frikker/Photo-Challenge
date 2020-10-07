ff3d03796c3a019d14684e7baaa58b09d5503fd9
Frikker <stolpnikk.97@gmail.com>
2020-10-08 02:48:33 +0300

    Now Ban reasons shown on your screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
       @photopost = current_user.photoposts.build if signed_in?
       @photoposts = @user.photoposts.order(aasm_state: :asc).page(params[:page])
       @banned_photoposts = @photoposts.where(aasm_state: 'banned')
+      flash[:danger] = "Твой аккаунт заблокирован по причине: #{@user.ban_reason}." if @user.banned?
       flash[:danger] = "У тебя #{@banned_photoposts.count} постов на удаление. Делай что-нибудь" if @banned_photoposts.any?
     else
       @photoposts = @user.photoposts.where.not(aasm_state: 'banned').order(id: :asc).page(params[:page])
